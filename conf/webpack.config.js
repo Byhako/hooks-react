12323ae55670ee185771b8316229296a284169b0
Ruben E Acosta <eldragondera1@gmail.com>
2020-12-27 18:54:16 -0500

    router

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/'
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -40,6 +41,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
+    historyApiFallback: true,
     port: 5000,
   },
 };
