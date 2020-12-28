7daf99d6e6cae87f3c9b834aabd8c6a1ae362da8
Ruben E Acosta <eldragondera1@gmail.com>
2020-12-27 19:30:39 -0500

    layout y estilos

---
+++ conf/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="footer-title">PlatziConf Merch</p>
+      <p className="footer-copy">&#174; Todos los derechos reservados</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ conf/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>PlatziConf Merch</h2>
+      <div className="header-checkout">
+        Checkout
+      </div>
+    </div>
+  );
+}
+
+export default Header;
