2c5e4151d64532582f38465e27220d488947b572
Ruben E Acosta <eldragondera1@gmail.com>
2020-12-30 19:19:23 -0500

    payment

--- conf/src/containers/Payment.jsx
+++ conf/src/containers/Payment.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '../styles/payment.css';
 
 const Payment = () => {
+  const { state: { cart } } = useContext(AppContext);
+
   return (
     <div className="payment">
       <section className="payment-content">
         <h3>Resumen del pedido</h3>
+        {cart.map(item => (
+          <div className="payment-item" key={item.title}>
+            <div className="payment-element">
+              <h4>{item.title}</h4>
+              <span>
+                $
+                {' '}
+                {item.price}
+              </span>
+            </div>
+          </div>
+        ))}
         <div className="payment-button">
-          Pago con paypal
+          <button type='button'>
+            Pagar
+          </button>
         </div>
       </section>
     </div>
